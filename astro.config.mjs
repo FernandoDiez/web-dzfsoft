16c074fa789fdea874db6e03ab7572f800e61fd7
FernandoDiez <dzfsoft@gmail.com>
2026-09-19 09:22:58 +0200

    Add Tailwind CSS integration and update homepage layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://tu-dominio.com",
+  site: "https://dzfsoft.es",
   base: "/",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
